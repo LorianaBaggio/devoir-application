156432307792b56b19250ed9d121b1c00396f2f6
YacineSoussi <50947823+YacineSoussi@users.noreply.github.com>
2021-04-15 14:20:17 +0200

    debut authentification

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { LoadingController } from '@ionic/angular';
+import { AuthService } from '../../services/auth.service';
+import { UserRegister } from '../../interfaces/user-register';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +13,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  isErrorMail: boolean = true;
+  isErrorPhone: boolean = true;
+  user: UserRegister = {first_name: '', last_name: '', email: '', password: '', username: ''};
+
+  constructor( 
+    private router: Router,
+    private auth: AuthService,
+    private toast: ToastController,
+    private loading: LoadingController,) { }
 
   ngOnInit() {
   }
+
+  checkEmail() {
+    const regex = new RegExp(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g);
+    this.isErrorMail = (regex.test(this.user.email.trim())) ? false : true;
+}
+
+async register() {
+  const load = await this.loading.create({
+      message: 'Please wait...',
+  });
+  await load.present();
+  // this.user.username = this.user.email.split('@')[0];
+  this.auth.register(this.user).then(async(data) => {
+      console.log(data);
+      await this.loading.dismiss();
+      this.router.navigate(['/login']);
+  }).catch(async(err) => {
+      console.log("error")
+      console.log(err.HttpErrorResponse.error);
+      const toast = await this.toast.create({
+          message: err,
+          duration: 2000
+      });
+      toast.present();
+      await this.loading.dismiss();
+  })
+}
+
 
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+
+import { UserRegister } from '../interfaces/user-register';
+
+// import { NativeStorage } from '@ionic-native/native-storage/ngx';
+
+import { Router } from '@angular/router';
+
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  url: string = 'http://localhost:8080/api'; // aaaaaaa@aaaaaaa.fr
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+ async login(email: string, password: string) {
+      return new Promise((resolve, rejects) => {
+          this.http.post(this.url + '/users/login', { email: email, password: password }).subscribe((data: any) => {
+              console.log(data);     
+                  localStorage.setItem('token', data.token);
+                  
+              (!data.success) ? rejects(false): resolve(data);
+              
+          });
+      });
+  }
+
+  register(user: UserRegister) {
+      return new Promise((resolve, rejects) => {
+          this.http.post(this.url + '/users/register', user).subscribe((data: any) => {
+              (!data.success) ? rejects(data.message): resolve(data);
+              
+          });
+      });
+  }
+
+  getProfile() {
+      return this.http.get(this.url + '/profil');
+  }
+}
