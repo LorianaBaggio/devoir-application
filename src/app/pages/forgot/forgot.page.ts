ccc26b4e216a5abd7eccb7f5d245753fb8d52449
Loriana Baggio <lbaggio@itnetwork.fr>
2021-04-21 10:58:06 +0200

    red line when email is not correct on forgot's page but green line if it is correct, this function removed on login's page

--- src/app/pages/forgot/forgot.page.ts
+++ src/app/pages/forgot/forgot.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserRegister } from '../../interfaces/user-register';
 
 @Component({
   selector: 'app-forgot',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotPage implements OnInit {
 
+  isErrorMail: boolean = true;  
+  user: UserRegister = {first_name: '', last_name: '', email: '', password: '', username: '', confirm_password: '' };
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  checkEmail() {
+    const regex = new RegExp(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g);
+    this.isErrorMail = (regex.test(this.user.email.trim())) ? false : true;
+}
 
 }
