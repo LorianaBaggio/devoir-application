5b8860b0dbc712688b3b4c2694c69f2f453d7b95
YacineSoussi <50947823+YacineSoussi@users.noreply.github.com>
2021-04-29 16:37:55 +0200

    npm/yarn install

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -64,7 +64,7 @@ export class AuthService {
                 } else {
                     loading.dismiss();
                     console.log(data);
-                    this.router.navigate(['/profil']); 
+                    this.router.navigate(['/categories']); 
                 }
             })
             .catch(error => {
